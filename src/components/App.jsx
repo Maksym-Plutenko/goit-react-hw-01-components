6571e08e5bfab0e927ace541fb064429e74f6c44
Maksym-Plutenko <plutenkom@gmail.com>
2023-02-25 00:24:31 +0200

    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,9 @@ import data from '../data/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
 
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -32,6 +35,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+const TransactionHistory = props => (
+  <table className="transaction-history">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {props.items.map(item => (
+            <tr key={item.id}>
+              <td>{item.invoice}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default TransactionHistory;
