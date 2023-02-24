e6cbf2db57c2608c79a0670a6a5c56d3dea8e0ba
Maksym-Plutenko <plutenkom@gmail.com>
2023-02-24 23:20:31 +0200

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import user from '../data/user.json';
 import Statistics from './Statistics/Statistics';
 import data from '../data/data.json';
 
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json';
+
 export const App = () => {
   return (
     <div
@@ -27,6 +30,8 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />;
     </div>
   );
 };
