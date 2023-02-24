b507fd154a0f047d95420194dc95a025d4645cf1
Maksym-Plutenko <plutenkom@gmail.com>
2023-02-24 22:35:21 +0200

    profile and statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +13,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* React homework template */}
+
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
