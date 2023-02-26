ddeae3ebe11689958767056b334d0ebfd4b9af71
Maksym-Plutenko <plutenkom@gmail.com>
2023-02-26 16:14:20 +0200

    stats random colors

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,9 +7,9 @@ const Statistics = ({ title, stats }) => (
     {title && <h2 className={css.title}>{title}</h2>}
     <ul className={css.statList}>
       {stats.map(stat => (
-        <li className={css.item} key={stat.id}>
+        <li className={css.item} key={stat.id} style={{backgroundColor: `#${Math.floor(Math.random()*16777215).toString(16)}` }}>
           <span className={css.label}>{stat.label}</span>
-          <span className={css.percentage}>{stat.percentage}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
         </li>
       ))}
     </ul>
